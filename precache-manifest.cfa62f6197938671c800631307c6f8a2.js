e6b38a1deee7a3a71b3fd42253e24355a191b0cb
fernandolievano <fernandolievan@gmail.com>
2019-05-23 23:30:37 -0300

    Updates

---
+++ precache-manifest.cfa62f6197938671c800631307c6f8a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76248aa4440873c62637335070c9a74",
+    "url": "/react-movies/index.html"
+  },
+  {
+    "revision": "3a350c8b6eddb00fe6a9",
+    "url": "/react-movies/static/css/2.62908225.chunk.css"
+  },
+  {
+    "revision": "d18e2fd86882ec9ea3c7",
+    "url": "/react-movies/static/css/main.79ad26e4.chunk.css"
+  },
+  {
+    "revision": "3a350c8b6eddb00fe6a9",
+    "url": "/react-movies/static/js/2.bb7454b2.chunk.js"
+  },
+  {
+    "revision": "d18e2fd86882ec9ea3c7",
+    "url": "/react-movies/static/js/main.40fc1ab4.chunk.js"
+  },
+  {
+    "revision": "cdb8474b72bd7dff1c63",
+    "url": "/react-movies/static/js/runtime~main.bf100f6d.js"
+  }
+]);
